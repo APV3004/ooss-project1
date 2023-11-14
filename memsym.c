da3311d0b0109922b032e8b1608d767fae6f0a2e
Adrián Pazos Varela <pazosvarelaa@gmail.com>
2023-11-14 17:42:30 +0100

    I have uploaded the first structure that is provided in the exercise in order to test how git and github work

--- memsym.c
+++ memsym.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAM_LINEA 16
 
+typedef struct {
+    unsigned char ETQ;
+    unsigned char Data[TAM_LINEA];
+    } T_CACHE_LINE;
