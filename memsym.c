221f7d424dd87be325cf32eea85a4b9b2d2c2218
Adrián Pazos Varela <pazosvarelaa@gmail.com>
2023-12-02 11:28:22 +0100

    We have created the DumpCACHE function which dumps the contents of the cache and renamed some variables

--- memsym.c
+++ memsym.c
@@ -5,6 +5,10 @@
 #define NUM_ROWS 8
 #define MEM_SIZE 4096
 
+typedef struct {
+    unsigned char ETQ;
+    unsigned char Data[TAM_LINEA];
+    } T_CACHE_LINE;
 
 
 void CleanCACHE(T_CACHE_LINE tbl[NUM_ROWS]);
@@ -12,21 +16,18 @@ void DumpCACHE(T_CACHE_LINE *tbl);
 void ParseAdress(unsigned int addr, int *LABEL, int*word, int *line, int *block);
 void TreatFailureMiss(T_CACHE_LINE *tbl, char *MRAM, int LABEL,int line, int block);
 
-typedef struct {
-    unsigned char ETQ;
-    unsigned char Data[TAM_LINEA];
-    } T_CACHE_LINE;
+
 
 // Global variables
 int globaltime = 0;
 int numerrors = 0;
 
 // Function to initialize the cache
-void CleanCACHE(T_CACHE_LINE cache[NUM_ROWS]) {
+void CleanCACHE(T_CACHE_LINE tbl[NUM_ROWS]) {
     for (int i = 0; i < NUM_ROWS; i++) {
-        cache[i].ETQ = 0xFF; // Initialize Label field to 0xFF
+        tbl[i].ETQ = 0xFF; // Initialize Label field to 0xFF
         for (int j = 0; j < TAM_LINEA; j++) {
-            cache[i].Data[j] = 0x23; // Initialize data field to 0x23
+            tbl[i].Data[j] = 0x23; // Initialize data field to 0x23
         }
     }
 }
